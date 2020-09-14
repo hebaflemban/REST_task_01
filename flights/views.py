345c59e63bd377bbacb778fe74aca4fb49fae75d
Heba <36724067+heba@users.noreply.github.com>
2020-09-14 11:55:07 +0300

    adds an api list view for flights and bookings

--- flights/views.py
+++ flights/views.py
@@ -1,1 +1,14 @@
 from .models import Flight, Booking
+from .serializers import FlightSerializer, BookingSerializer
+from rest_framework.generics import ListAPIView
+import datetime
+from django.utils import timezone
+
+
+class FlightsList(ListAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+class Bookings(ListAPIView):
+    queryset = Booking.objects.filter(date__gt = datetime.date.today() )
+    serializer_class = BookingSerializer
